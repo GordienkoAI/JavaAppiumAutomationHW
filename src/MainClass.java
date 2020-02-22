d3502b993a314cc403295c5878742d8a7d4dac33
gordienko_ani <gordienko.ani@open.ru>
2020-02-22 10:26:46 +0300

    Home Work Test 2 and Test 3 add getClassString add getClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,21 @@
 public class MainClass {
 
-    public static int getLocalNumber(){
+    private int class_number = 20;
+    private String calss_string = "Hello World";
+
+
+    public String getClassString()
+    {
+        return calss_string;
+    }
+
+    public int getClassNumber()
+    {
+        return class_number;
+    }
+
+    public static int getLocalNumber()
+    {
         return 14;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,17 +1,35 @@
 import org.junit.Assert;
 import org.junit.Test;
+import sun.applet.Main;
 
 public class MainClassTest {
 
+    // Домашнее задание Тест 1
     @Test
     public void testGetLocalNumber(){
         Assert.assertTrue("Метод MainClass вернул число не равное 14",MainClass.getLocalNumber() == 14);
     }
-
 
     @Test
     public void secondTestGetLocalNumber(){
         Assert.assertTrue("Метод MainClass вернул число не равное 14",MainClass.getLocalNumber() == 10);
     }
 
+    //ДДомашнее задание  Тест 2
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("Метод getClassNumber вернул число меньшее 45", MainClass.getLocalNumber() > 45);
+    }
+
+    //Домашнее задание Тест 3
+    @Test
+    public void testGetClassString()
+    {
+         MainClass mainObj = new MainClass();
+         String stringObj = mainObj.getClassString().substring(0,5).toLowerCase();
+
+
+        Assert.assertTrue("Переменная calss_string не содержит подстроку 'Hello' или 'hello'", stringObj.contains("hello"));
+    }
 }
